4f1d94a77ae98a2bfd6966509205953c96af5e3a
avysel <avanryssel@gmail.com>
2022-04-15 11:13:13 +0200

    fix: fix faucet layout

--- src/components/Faucet/FaucetToWalletRequest.tsx
+++ src/components/Faucet/FaucetToWalletRequest.tsx
@@ -1,4 +1,5 @@
 import { TezosToolkit } from "@taquito/taquito";
+import { Row, Col } from "react-bootstrap";
 import FaucetRequestButton from "./FaucetRequestButton";
 import FaucetSendButton from "./FaucetSendButton";
 import UserInfo from "./UserInfo";
@@ -7,13 +8,18 @@ function FaucetToWalletRequest({ user, network, status, Tezos }: { user: any, ne
 
     return (
         <>
-            <UserInfo user={user} displayBalance={true}/>
-            <FaucetRequestButton network={network} to={user.userAddress} status={status} />
-            &nbsp;
-            {network.allowSendButton &&
+            <div className="faucet-part-user">
+                <UserInfo user={user} displayBalance={true} />
+            </div>
 
-                <FaucetSendButton network={network} user={user} Tezos={Tezos} status={status} />
-            }
+            <div>
+                <FaucetRequestButton network={network} to={user.userAddress} status={status} />
+                &nbsp;
+                {network.allowSendButton &&
+
+                    <FaucetSendButton network={network} user={user} Tezos={Tezos} status={status} />
+                }
+            </div>
         </>
     )
 }
