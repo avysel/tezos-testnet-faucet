8d6118b001e9882658645a40e22b1c48bc1b7ba6
avysel <avanryssel@gmail.com>
2022-04-15 10:07:20 +0200

    feat: copy faucet address button

--- src/components/Faucet/FaucetInfo.tsx
+++ src/components/Faucet/FaucetInfo.tsx
@@ -1,15 +1,40 @@
-import { Badge, Card } from "react-bootstrap";
-import { DropletFill } from "react-bootstrap-icons";
+import { useState } from "react";
+import { Badge, Card, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { DropletFill, Files } from "react-bootstrap-icons";
 import { toBalance } from "../../lib/Utils";
 
-function FaucetInfo({faucetAddress, faucetBalance} : {faucetAddress: string, faucetBalance: number}) {
+function FaucetInfo({ faucetAddress, faucetBalance }: { faucetAddress: string, faucetBalance: number }) {
+
+    const [tooltipLabel, setTooltipLabel] = useState<string>("Click to copy faucet address");
+
+    const copy = () => {
+        navigator.clipboard.writeText(faucetAddress).then(() => {
+            setTooltipLabel("Copied!");
+        })
+        setTimeout(() => {
+            setTooltipLabel("Click to copy faucet address");
+        }, 2000);
+    }
+
     return (
         <>
-            <Card.Text  className="faucet-info">
+            <Card.Text className="faucet-info">
 
                 <Badge bg="light" text="dark" className="balance-badge">
-                        <DropletFill /> &nbsp; {faucetAddress} &nbsp;                        
-                        <Badge bg="secondary" as="span" className="balance-badge">{toBalance(faucetBalance)} ꜩ</Badge>                    
+                    <DropletFill /> &nbsp; {faucetAddress} &nbsp;
+                    <Badge bg="secondary" as="span" className="balance-badge">{toBalance(faucetBalance)} ꜩ</Badge>
+                    &nbsp;
+                    <OverlayTrigger
+                        key="right"
+                        placement="right"
+                        overlay={
+                            <Tooltip id={`tooltip-right`}>
+                                {tooltipLabel}
+                            </Tooltip>
+                        }
+                    >
+                        <Files onClick={copy} />
+                    </OverlayTrigger>
                 </Badge>
 
             </Card.Text>
